e48f75d455f400d1820be2a4ae46e75a674a6eaa
Alexander Stekelenburg <anoniem.axander@gmail.com>
2017-07-29 21:20:19 +0200

    More information!

--- src/main/java/alexanders/mods/inspector/Inspector.java
+++ src/main/java/alexanders/mods/inspector/Inspector.java
@@ -1,34 +1,27 @@
 package alexanders.mods.inspector;
 
-import de.ellpeck.rockbottom.api.GameContent;
 import de.ellpeck.rockbottom.api.IApiHandler;
 import de.ellpeck.rockbottom.api.IGameInstance;
 import de.ellpeck.rockbottom.api.RockBottomAPI;
-import de.ellpeck.rockbottom.api.assets.IAssetManager;
-import de.ellpeck.rockbottom.api.construction.BasicRecipe;
 import de.ellpeck.rockbottom.api.data.settings.Keybind;
 import de.ellpeck.rockbottom.api.event.IEventHandler;
 import de.ellpeck.rockbottom.api.event.impl.OverlayRenderEvent;
-import de.ellpeck.rockbottom.api.event.impl.WorldRenderEvent;
 import de.ellpeck.rockbottom.api.item.ItemBasic;
-import de.ellpeck.rockbottom.api.item.ItemInstance;
 import de.ellpeck.rockbottom.api.mod.IMod;
-import de.ellpeck.rockbottom.api.tile.TileBasic;
 import de.ellpeck.rockbottom.api.util.reg.IResourceName;
 import org.newdawn.slick.Input;
-import org.newdawn.slick.util.Log;
 
 import static de.ellpeck.rockbottom.api.RockBottomAPI.KEYBIND_REGISTRY;
 import static de.ellpeck.rockbottom.api.RockBottomAPI.createRes;
 
-public class Inspector implements IMod{
-    
+public class Inspector implements IMod {
+
     public static Inspector instance;
     public static ItemBasic inspectorItem;
     public static IResourceName inspectorName;
     public static Keybind keybind;
-    
-    
+
+
     public Inspector() {
         instance = this;
         IResourceName keybindName = createRes(this, "desc.keybind");
@@ -36,34 +29,34 @@ public class Inspector implements IMod{
     }
 
     @Override
-    public String getDisplayName(){
+    public String getDisplayName() {
         return "Inspector";
     }
 
     @Override
-    public String getId(){
+    public String getId() {
         return "inspector";
     }
 
     @Override
-    public String getVersion(){
+    public String getVersion() {
         return "@VERSION@";
     }
 
     @Override
-    public String getResourceLocation(){
-        return "/assets/"+getId();
+    public String getResourceLocation() {
+        return "/assets/" + getId();
     }
 
     @Override
-    public String getDescription(){
+    public String getDescription() {
         return RockBottomAPI.getGame().getAssetManager().localize(createRes(this, "desc.mod"));
     }
 
     @Override
-    public void init(IGameInstance game, IApiHandler apiHandler, IEventHandler eventHandler){
+    public void init(IGameInstance game, IApiHandler apiHandler, IEventHandler eventHandler) {
         inspectorItem = new ItemBasic(inspectorName = createRes(this, "inspector"));
         inspectorItem.register();
-        eventHandler.registerListener(OverlayRenderEvent.class, new InspectorRenderer());   
+        eventHandler.registerListener(OverlayRenderEvent.class, new InspectorRenderer());
     }
 }
--- src/main/java/alexanders/mods/inspector/InspectorRenderer.java
+++ src/main/java/alexanders/mods/inspector/InspectorRenderer.java
@@ -12,7 +12,10 @@ import de.ellpeck.rockbottom.api.event.IEventListener;
 import de.ellpeck.rockbottom.api.event.impl.OverlayRenderEvent;
 import de.ellpeck.rockbottom.api.event.impl.TooltipEvent;
 import de.ellpeck.rockbottom.api.item.ItemInstance;
+import de.ellpeck.rockbottom.api.item.ToolType;
 import de.ellpeck.rockbottom.api.tile.Tile;
+import de.ellpeck.rockbottom.api.tile.entity.TileEntity;
+import de.ellpeck.rockbottom.api.tile.entity.TileEntityFueled;
 import de.ellpeck.rockbottom.api.tile.state.TileState;
 import de.ellpeck.rockbottom.api.util.BoundBox;
 import de.ellpeck.rockbottom.api.util.Util;
@@ -21,12 +24,15 @@ import de.ellpeck.rockbottom.api.world.TileLayer;
 import org.newdawn.slick.Graphics;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
-import static de.ellpeck.rockbottom.api.RockBottomAPI.getApiHandler;
-import static de.ellpeck.rockbottom.api.RockBottomAPI.getEventHandler;
+import static de.ellpeck.rockbottom.api.RockBottomAPI.*;
 
 public class InspectorRenderer implements IEventListener<OverlayRenderEvent> {
+    private HashMap<Tile, List<ItemInstance>> dropLists = new HashMap<>();
+
+
     @Override
     public EventResult listen(EventResult result, OverlayRenderEvent event) {
         ItemInstance selected;
@@ -45,15 +51,17 @@ public class InspectorRenderer implements IEventListener<OverlayRenderEvent> {
                     int x = Util.floor(mouseX);
                     int y = Util.floor(mouseY);
                     List<Entity> entities = world.getEntities(new BoundBox(mouseX, mouseY, mouseX, mouseY));
-                    if(entities.size() > 0) {
-                        describeEntity(entities.get(0), mouseX, mouseY, event.assetManager, event.graphics, game);
-                    }else {
+                    if (entities.size() > 0) {
+                        describeEntity(entities.get(0), event.assetManager, event.graphics, game);
+                    } else {
                         TileState tile;
-                        if (Settings.KEY_BACKGROUND.isDown())
+                        if (Settings.KEY_BACKGROUND.isDown()) {
                             tile = world.getState(TileLayer.BACKGROUND, x, y);
-                        else
+                            describeTile(tile, x, y, TileLayer.BACKGROUND, world, event.assetManager, event.graphics, game, player);
+                        } else {
                             tile = world.getState(TileLayer.MAIN, x, y);
-                        describeTile(tile, x, y, world, event.assetManager, event.graphics, game);
+                            describeTile(tile, x, y, TileLayer.MAIN, world, event.assetManager, event.graphics, game, player);
+                        }
                     }
                 }
             }
@@ -61,23 +69,35 @@ public class InspectorRenderer implements IEventListener<OverlayRenderEvent> {
         return result;
     }
 
-    private void describeEntity(Entity entity, double x, double y, IAssetManager manager, Graphics graphics, IGameInstance game) {
+    private void describeEntity(Entity entity, IAssetManager manager, Graphics graphics, IGameInstance game) {
         List<String> desc = new ArrayList<>();
-        desc.add("UUID: "+entity.getUniqueId());
-        desc.add("Position: "+ String.format("%.3f : %.3f", entity.x, entity.y));
-        if(entity instanceof EntityItem) {
+        desc.add("UUID: " + entity.getUniqueId());
+        desc.add("Position: " + String.format("%.3f : %.3f", entity.x, entity.y));
+        if (entity.getAdditionalData() != null)
+            desc.add("Additional data: " + entity.getAdditionalData());
+        desc.add("Seconds existed: " + entity.ticksExisted / 40);
+        desc.add("Motion x axis: " + String.format("%.3f", entity.motionX));
+        desc.add("Motion y axis: " + String.format("%.3f", entity.motionY));
+        if (entity instanceof EntityItem) {
             ((EntityItem) entity).item.getItem().describeItem(manager, ((EntityItem) entity).item, desc, true);
-        }else if(entity instanceof EntityLiving) {
-            desc.add("Health: "+ ((EntityLiving) entity).getHealth() + " / "+ ((EntityLiving) entity).getMaxHealth());
-            desc.add("Regen rate: 1 HP every "+ ((EntityLiving) entity).getRegenRate()+ " ticks");
-            if(entity instanceof AbstractEntityPlayer) {
-                desc.add("Name: "+((AbstractEntityPlayer) entity).getName());
+            desc.add("Meta: " + ((EntityItem) entity).item.getItem());
+            desc.add("Amount: " + ((EntityItem) entity).item.getAmount() + " / " + ((EntityItem) entity).item.getMaxAmount());
+            if (((EntityItem) entity).item.getAdditionalData() != null)
+                desc.add("Item additional data" + ((EntityItem) entity).item.getAdditionalData());
+        } else if (entity instanceof EntityLiving) {
+            desc.add("Health: " + ((EntityLiving) entity).getHealth() + " / " + ((EntityLiving) entity).getMaxHealth());
+            desc.add("Regen rate: 1 HP every " + ((EntityLiving) entity).getRegenRate() + " ticks");
+            if (entity instanceof AbstractEntityPlayer) {
+                desc.add("");
+                desc.add("Name: " + ((AbstractEntityPlayer) entity).getName());
+                if (getNet().isActive())
+                    desc.add("Command level: " + ((AbstractEntityPlayer) entity).getCommandLevel());
             }
         }
         getApiHandler().drawHoverInfoAtMouse(game, manager, graphics, true, 500, desc);
     }
 
-    private void describeTile(TileState state, int x, int y, IWorld world, IAssetManager manager, Graphics graphics, IGameInstance game) {
+    private void describeTile(TileState state, int x, int y, TileLayer layer, IWorld world, IAssetManager manager, Graphics graphics, IGameInstance game, AbstractEntityPlayer player) {
         Tile tile = state.getTile();
         ItemInstance itemInstance = null;
         if (tile.getItem() != null)
@@ -87,12 +107,80 @@ public class InspectorRenderer implements IEventListener<OverlayRenderEvent> {
         desc.add("Name: " + (itemInstance == null ? manager.localize(tile.getName()) : itemInstance.getDisplayName()));
         desc.add("State: " + state.toString());
         desc.add("Position: " + x + " : " + y);
-        desc.add("Light: " + world.getCombinedLight(x, y));
+        desc.add("Sky light: " + world.getSkyLight(x, y));
+        desc.add("Artificial light: " + world.getArtificialLight(x, y));
+        desc.add("Combined light: " + world.getCombinedLight(x, y));
+        desc.add("Hardness: " + tile.getHardness(world, x, y, layer));
+        desc.add("Light emitted: " + tile.getLight(world, x, y, layer));
+        desc.add("Translucency: " + String.format("%.0f %%", tile.getTranslucentModifier(world, x, y, layer, true) * 100));
+        String effective = getEffective(tile, world, x, y, layer);
+        if (!effective.isEmpty())
+            desc.add("Effective tool type: " + effective);
+        if (tile.canProvideTileEntity()) {
+            TileEntity te = world.getTileEntity(x, y);
+            if (te != null) {
+                if (te instanceof TileEntityFueled) {
+                    desc.add("Active: " + ((TileEntityFueled) te).isActive());
+                    desc.add("Fuel time: " + ((TileEntityFueled) te).getFuelPercentage());
+                }
+            }
+        }
+        desc.add("Biome: " + world.getBiome(x, y).getName());
         if (itemInstance != null)
             tile.describeItem(manager, itemInstance, desc, true);
+
+        List<ItemInstance> drops = tile.getDrops(world, x, y, layer, player);
+        if (dropLists.containsKey(tile)) { //This is for reducing flickering
+            if (dropLists.get(tile) != null) {
+                if (dropLists.get(tile).equals(drops)) {
+                    if (!drops.isEmpty()) {
+                        desc.add("");
+                        desc.add("Drops:");
+                        for (ItemInstance drop : drops) {
+                            desc.add("\t" + drop.getDisplayName() + ": " + drop.getAmount());
+                        }
+                    }
+                } else {
+                    dropLists.put(tile, null);
+                }
+            }
+        } else {
+            if (drops != null && !drops.isEmpty()) {
+                dropLists.put(tile, drops);
+                desc.add("");
+                desc.add("Drops:");
+                for (ItemInstance drop : drops) {
+                    desc.add("\t" + drop.getDisplayName() + ": " + drop.getAmount());
+                }
+            }
+        }
+
 
         if (itemInstance == null || !getEventHandler().fireEvent(new TooltipEvent(itemInstance, game, manager, graphics, desc)).shouldCancel()) {
             getApiHandler().drawHoverInfoAtMouse(game, manager, graphics, true, 500, desc);
         }
+    }
+
+    private String getEffective(Tile tile, IWorld world, int x, int y, TileLayer layer) {
+        StringBuilder builder = new StringBuilder();
+        if (tile.isToolEffective(world, x, y, layer, ToolType.AXE, Integer.MAX_VALUE)) {
+            builder.append("Axe");
+        }
+        if (tile.isToolEffective(world, x, y, layer, ToolType.PICKAXE, Integer.MAX_VALUE)) {
+            if (builder.length() > 0)
+                builder.append(", ");
+            builder.append("Pickaxe");
+        }
+        if (tile.isToolEffective(world, x, y, layer, ToolType.SHOVEL, Integer.MAX_VALUE)) {
+            if (builder.length() > 0)
+                builder.append(", ");
+            builder.append("Shovel");
+        }
+        if (tile.isToolEffective(world, x, y, layer, ToolType.SWORD, Integer.MAX_VALUE)) {
+            if (builder.length() > 0)
+                builder.append(", ");
+            builder.append("Sword");
+        }
+        return builder.toString();
     }
 }
